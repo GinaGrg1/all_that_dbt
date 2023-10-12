800bba9aea8c624669795ce9f494a1d67810b49b
GurungRegina <gina524286@gmail.com>
2023-10-12 15:35:19 +0100

    Update custom_test.sql

--- minerva/tests/generic/custom_test.sql
+++ minerva/tests/generic/custom_test.sql
@@ -1,3 +1,5 @@
+-- This custom test name (custom_test) must be included in the schema.yml file under tests
+-- the parameters model & column_name will be passed from the schema.yml file.
 {%test custom_test(model, column_name) %}
 
 select *
