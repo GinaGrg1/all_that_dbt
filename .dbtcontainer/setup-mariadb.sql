7e8aea651aa33b2e445e1c0827c191d5883212c3
GurungRegina <gina524286@gmail.com>
2024-07-24 10:18:43 +0100

    Delete .dbtcontainer directory

--- .dbtcontainer/setup-mariadb.sql
+++
@@ -1,9 +0,0 @@
-USE mariadb;
--- Using the root user, grant access to the mariadb user.
-GRANT ALL PRIVILEGES ON * TO 'root' @'%' IDENTIFIED BY 'mariadb';
-
--- Example project
-CREATE OR REPLACE DATABASE example COMMENT 'example';
-
--- Intro project
-CREATE OR REPLACE DATABASE minerva COMMENT 'minerva';
--- .dbtcontainer/setup.sh
+++
@@ -1,2 +0,0 @@
-mysql -h 127.0.0.1 -uroot -pmariadb < .dbtcontainer/setup-mariadb.sql
-echo 'export DBT_PROFILES_DIR="/workspace"'  >> ~/.bash_profile
