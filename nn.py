7a531ef184b03f01eee042261d3eaff6f74beb73
GurungRegina <gina524286@gmail.com>
2025-04-24 19:23:09 +0100

    Update nn.py

--- nn.py
+++ nn.py
@@ -1,33 +1,36 @@
-def order_rules_by_dependency(unordered_rules: list[dict]) -> list[dict]:
-    """Orders dictionary items by required dependencies.
-    Creates a new list of dictionaries containing the same metadata rules
-    supplied in the unordered_rules parameter, but reordered to ensure that
-    any rule which is a dependency of another occurs in the list prior to encountering
-    the dependency. This makes sure that any rules which are required to be completed
-    are done so in the correct order.
+def topologicalSort(items):
 
-    Args:
-        unordered_rules (list[dict]): List of rules retrievied from the YAML metadata.
-    
-    Returns:
-        ordered_rules (list[dict]): List containing the same dictionaries supplied in
-                                    unordered_rules, but in a different order to make sure
-                                    rules required by later rules have already completed.
-    """
-        
-    ordered_rules = []
-    
-    # Repeatedly iterate the unordered rules removing them post-processing until the list is empty
-    while unordered_rules:
-        for index, rule in enumerate(unordered_rules):
-            # If the rule doesn't depend on another rule, or if it does but that rule is already in ordered_rules
-            #  then append the rule to ordered_rules and remove it from unordered_rules
-            if "depends_on" not in rule or any(dep_rule["name"] == rule["depends_on"] for dep_rule in ordered_rules):
+  nodes = {item["name"]: i for i, item in enumerate(items)}
+  edges = defaultdict(list)
+  in_degree = defaultdict(int)
 
-                ordered_rules.append(rule)
-                unordered_rules.pop(index)
-                
-                break
+  for item in items:
+    name = item["name"]
+    depends_on = item.get("depends_on", [])
+    for dep in depends_on:
+      edges[dep].append(name)
+      in_degree[name] += 1
 
+  to_visit = deque([node for node in nodes.keys() if in_degree[node] == 0])
+  topo_sorted_idxs = []
 
-    return ordered_rules
+  while to_visit:
+    node = to_visit.popleft()
+    topo_sorted_idxs.append(nodes[node])
+    # print("visiting: ", node, "to_visit: ", to_visit)
+    for parent in edges[node]:
+      in_degree[parent] -= 1
+      if in_degree[parent] == 0:
+          # print("-- adding parent: ", parent)
+          to_visit.append(parent)
+
+  if len(topo_sorted_idxs) != len(nodes):
+    raise ValueError("cycle detected")
+
+  ordered_rules = [
+      data[idx] for idx in topo_sorted_idxs
+  ]
+
+  return ordered_rules
+
+topologicalSort(data)
